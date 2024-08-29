69d16e9d06f1bba59c9e4a7f2e1035266822ad87
csbobo <751541594@qq.com>
2024-08-29 09:05:51 +0800

    提交脚本

--- Server/llama.cpp/1_编译_cpu.bat
+++ Server/llama.cpp/1_编译_cpu.bat
@@ -5,6 +5,6 @@ chcp 65001
 
 CALL C:\ProgramData\anaconda3\Scripts\activate.bat C:\Users\75154\.conda\envs\llama_cpp
 
-CALL cmake --build build_gpu --config Release -j
+CALL cmake -B build_cpu && cmake --build build_cpu --config Release -j
 
 CALL pause
